f797980c4e9faebf6abb6327515eb5a743067415
Vlad <wlados@gmail.com>
2018-07-27 17:29:42 +0300

    это первый  коммит

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function myFirstApp(name, age) {
+
+	alert('Привет, меня зовут ' + name + ' и это моя первая программа');
+
+	function showSkills() {
+		let skills = ['html', 'css', 'javascript', 'python'];
+
+		for (let i = 0; i < skills.length; i++) {
+			document.write("Я владею " + skills[i] + "<br>");
+		}
+	}	
+	showSkills();
+
+	function checkAge() {
+		if (age > 18) {
+				alert("Добро пожаловать!");
+		} else {
+			alert("Вход запрещен!");
+		}
+	}
+	checkAge();
+	function calcPow(num) {
+		alert(num + " в квадрате = " + num*num);
+	}
+	calcPow(4);
+}
+
+myFirstApp("Vlad", 36);
